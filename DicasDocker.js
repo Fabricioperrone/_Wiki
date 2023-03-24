aa61961422dfb0c8d474260af7d59c7786e1f93d
Fabricio Acer <fabricio_luis@outlook.com>
2023-03-24 19:20:55 -0300

    Dica docker

---
+++ DicasDocker.js
@@ -0,0 +1,6 @@
+//  Para verificar as versões do Docker, git, Node.js e npm instaladas em seu computador, execute o comando docker --version && git --version && node --version && npm --version no seu terminal de linha de comando.
+// Com o docker rodando em seu computador, execute o 
+// comando 
+// docker run --publish 80:80 --publish 22:22 --hostname localhost wlsf82/gitlab-ce 
+//e aguarde até o ambiente inicializar.
+
